5da9af0756e4d01c925085d9f7a18ea5a0371887
Lucasabdala94 <83674006+Lucasabdala94@users.noreply.github.com>
2022-10-10 04:18:48 -0300

    2 files modified

--- src/App.js
+++ src/App.js
@@ -2,34 +2,13 @@ import React, { useState } from 'react';
 import './App.css';
 import Alarms from './components/Alarms';
 import ModalAlarm from './components/ModalAlarm';
-import { db } from './firebase';
-import { getDocs, collection } from 'firebase/firestore';
 
 function App() {
-  const [alarmas, setAlarmas] = useState([]);
-  let alarmasTodas = [];
-  (async () => {
-    try {
-      const getAlarm = await getDocs(collection(db, '/alarmas'));
-      getAlarm.forEach((doc) => {
-        alarmasTodas.push({
-          id: doc.id,
-          alarma: doc.data().alarma,
-          descripcion: doc.data().descripcion,
-          et: doc.data().et,
-          nivelTension: doc.data().nivelTension,
-        });
-      });
-    } catch (e) {
-      console.log(e);
-    }
-  })();
-  setAlarmas(alarmasTodas);
   return (
     <div className="App">
       <h1>Registro de Mediciones y Alarmas Erroneos</h1>
       <ModalAlarm />
-      <Alarms alarmas={alarmas} />
+      <Alarms />
     </div>
   );
 }
--- src/components/Alarms.js
+++ src/components/Alarms.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Input } from 'semantic-ui-react';
+import { db } from './../firebase';
+import { getDocs, collection } from 'firebase/firestore';
 
 export default function Alarms(props) {
-  const { Alarmas } = props;
-  console.log(Alarmas);
+  let alarmasTodas = [];
+  (async () => {
+    try {
+      const getAlarm = await getDocs(collection(db, '/alarmas'));
+      getAlarm.forEach((doc) => {
+        alarmasTodas.push({
+          id: doc.id,
+          alarma: doc.data().alarma,
+          descripcion: doc.data().descripcion,
+          et: doc.data().et,
+          nivelTension: doc.data().nivelTension,
+        });
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  })();
+  console.log(alarmasTodas[0]);
   return (
     <div>
       <h1>Buscador de Alarmas</h1>
@@ -14,7 +32,6 @@ export default function Alarms(props) {
           placeholder="Buscar Alarmas..."
         />
       </div>
-      <div></div>
     </div>
   );
 }
