365193966903e2619a38969211ac9a963b42a6a2
Lucasabdala94 <83674006+Lucasabdala94@users.noreply.github.com>
2022-10-10 03:44:57 -0300

    agregada consulta

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import React from 'react';
 import './App.css';
 import Alarms from './components/Alarms';
 import ModalAlarm from './components/ModalAlarm';
+import { db } from './firebase';
+import { getDocs, collection } from 'firebase/firestore';
 
 function App() {
+  let alarmasTodas = [];
   (async () => {
-    let alarmasTodas = [];
-    
+    try {
+      const getAlarm = await getDocs(collection(db, '/alarmas'));
+      getAlarm.forEach((doc) => {
+        alarmasTodas.push({
+          id: doc.id,
+          alarma: doc.data().alarma,
+          descripcion: doc.data().descripcion,
+          et: doc.data().et,
+          nivelTension: doc.data().nivelTension,
+        });
+      });
+    } catch (e) {
+      console.log(e);
+    }
   })();
-
+  console.log(alarmasTodas);
   return (
     <div className="App">
       <h1>Registro de Mediciones y Alarmas Erroneos</h1>
