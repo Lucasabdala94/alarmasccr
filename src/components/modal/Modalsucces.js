aa7f1cf19412df0bbaad6d049faf6a76e240e6a3
lucas <lucasabdala94@gmail.com>
2022-12-22 21:19:00 -0300

    solucionado problemas

--- src/components/modal/Modalsucces.js
+++ src/components/modal/Modalsucces.js
@@ -12,7 +12,7 @@ export default function ModalError(props) {
       size="small"
       blurring="true"
     >
-<<<<<<< HEAD
+
       <Header className='Modal-header'>
         <div className='Content-Icon'><Icon name="check square outline" color="green" /></div>
         <p className='Modal-title'>Alarma registrada correctamente</p>
@@ -21,15 +21,6 @@ export default function ModalError(props) {
         <Button inverted  className='Modal-header' color="green" onClick={onClose}>
           <div className='Modal-title'><Icon name="check circle outline" /></div>
           <p className='Content-Icon'>Volver</p>
-=======
-      <Header className="header-succes">
-        <h4 className='title-succes'>Alarma registrada correctamente</h4>
-        <Icon name="check square outline" color="green" />
-      </Header>
-      <Modal.Actions className='content-succes'>
-        <Button className='button-succes' basic color="green" onClick={onClose}>
-          <Icon className='content-button-succes' name="check circle outline" /> Volver
->>>>>>> 92b5ffff67973c976061568db4e4457d56fa73a4
         </Button>
       </Modal.Actions>
     </Modal>
