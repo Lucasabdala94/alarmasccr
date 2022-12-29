42d37d257c0c86639a654dddf42fb2eb52286538
lucas <lucasabdala94@gmail.com>
2022-12-29 15:54:37 -0300

    solucionado filterbuscador

--- src/helper/filtroBusqueda.js
+++ src/helper/filtroBusqueda.js
@@ -1,10 +1,6 @@
 //crea un array con las alarmas que coinciden con busquedas.
 
 export async function filtroBusqueda(alarmas, busqueda) {
-    let resultado = await alarmas?.filter((alarma) => {
-      if (alarma?.data?.et.includes(busqueda)) {
-        return alarma;
-      }
-    });
+    let resultado = alarmas.filter(alarma => alarma?.data?.et.includes(busqueda));
     return resultado;
 }
